63eb657477b27717c2c8c8957997a50700d87203
alex <alex@ajofreerange.com>
2024-12-02 14:13:48 -0700

    source maps backend

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
+  webpack: (config, {isServer}) => {
+    if (isServer) {
+      config.devtool = 'source-map'
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
